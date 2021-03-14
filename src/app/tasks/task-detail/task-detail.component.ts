eef3d695ef512781f24d9ea27f1fa28b9a2c71d4
RicardoBap <ric_bap@hotmail.com>
2021-03-14 15:10:29 -0300

    Adding navigation between TaskComponent and TaskDetailComponent

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
+import { Location } from "@angular/common";
 
 import { Task } from './../shared/task.model'
 import { TaskService } from './../shared/task.service';
@@ -11,14 +12,21 @@ import 'rxjs/add/operator/switchMap'
 })
 export class TaskDetailComponent implements OnInit {
 
+  public task: Task
+
   constructor(
     private taskService: TaskService,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute,
+    private location: Location) {}
 
   public ngOnInit() {
     this.route.params
       .switchMap( (params: Params) => this.taskService.getTask(+params['id']) )
       .subscribe( _task => this.task = _task )     
+  }
+
+  public goBack() {
+    this.location.back()
   }
 
 }
