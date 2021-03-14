fc1d9632c032cc17b61f110056ab56fc07e25fd6
RicardoBap <ric_bap@hotmail.com>
2021-03-14 09:16:37 -0300

    Adding basic routes config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+
+const ROUTES = RouterModule.forRoot([
+  { path: 'tasks', component: TasksComponent }
+])
 
 @NgModule({
   declarations: [
@@ -18,7 +23,9 @@ import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
