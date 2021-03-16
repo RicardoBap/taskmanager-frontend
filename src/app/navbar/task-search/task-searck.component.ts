40bbb107ef0d241eaa58eff085db435ac647c32d
RicardoBap <ric_bap@hotmail.com>
2021-03-16 11:12:15 -0300

    Adding the task search component logiv

--- src/app/navbar/task-search/task-searck.component.ts
+++ src/app/navbar/task-search/task-searck.component.ts
@@ -1,16 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 import { Task } from './../../tasks/shared/task.model';
 import { TaskService } from './../../tasks/shared/task.service';
 
+import { Observable } from 'rxjs/Observable';
+import { Subject } from "rxjs/Subject"
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/Observable/of'
+
+ 
 @Component({
   selector: 'app-task-search',
   templateUrl: './task-search.component.html'
 })
-export class TaskSearchComponent {
+export class TaskSearchComponent implements OnInit {
 
-  public task: Task
+  public searchTerms: Subject<any> = new Subject()
+  public tasks: Task[] = []
 
-  public constructor(private taskSerice: TaskService) {}
+  public constructor(
+    private taskService: TaskService,
+    private route: Router) {}
+
+  public ngOnInit() {
+    this.searchTerms.switchMap(
+      term => term ? this.taskService.searchByTitle(term) : Observable.of<Task[]>([])
+    ).subscribe(tasks => this.tasks = tasks)
+  }
+
+  public search(term: string) {
+    this.searchTerms.next(term)
+  }
+
+  public goToTask(task: Task) {
+    this.tasks = []
+    this.route.navigate(['/tasks', task.id])
+  }
 
 }
