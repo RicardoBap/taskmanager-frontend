def4af57e4267f98534f9e53a4168917fd967241
RicardoBap <ric_bap@hotmail.com>
2021-03-17 09:30:22 -0300

    Adding new fields to task

--- src/app/tasks/shared/task.model.ts
+++ src/app/tasks/shared/task.model.ts
@@ -1,9 +1,25 @@
 export class Task {
   public id: number
   public title: string
+  public description: string
+  public done: boolean
+  public deadline: string
 
-  constructor(id: number, title: string) {
+  constructor(id: number, title: string, description?: string, done?: boolean, deadline?: string) {
     this.id = id
     this.title = title
+    this.description = description
+    this.done = done
+    this.deadline = deadline
   }
 }
+
+// ?(elvis) que o campo não é obrigatório
+// export class Task {
+//   constructor(
+//     public id: number,
+//     public title: string,
+//     public description?: string,
+//     public done?: boolean,
+//     public deadline?: string) {}  
+// }
