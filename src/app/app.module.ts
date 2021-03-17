453a7d4f1d544adb4618c8abd94395de4026d469
RicardoBap <ric_bap@hotmail.com>
2021-03-17 17:12:41 -0300

    Adding basic setup to use reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 // Ordem alfabética
@@ -46,6 +46,7 @@ import * as datetimepicker from 'eonasdan-bootstrap-datetimepicker'
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule,
     InMemoryWebApiModule.forRoot(InMemoryTaskDataService)
